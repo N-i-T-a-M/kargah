0ff559a1879198e993a8e3619456f25713fe0f00
N-i-T-a-M <matin.mathematics@gmail.com>
2022-12-03 11:31:22 +0330

    this is my second commit

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 int taghsim (int x,int y){
     return x/y;
 }
+int zarb (int x){
+    return 2*x;
+}
 int main(){
     int  a;
     scanf("%d",&a);
-    int t = taghsim(a+a,3);
+    int t = zarb(taghsim(a+a,3));
     printf("%d",t);
 }
